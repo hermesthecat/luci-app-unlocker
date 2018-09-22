da7d8d0931b4d7bd61f03d5444d6b0113d31c7ba
Nooblord <79151006869@live.ru>
2018-09-22 19:01:23 +0300

    Added custom ipv4 with subnet support, added CRON list auto-update, added http-only support, ready for general use with RKN and Tor

--- files/luci/model/cbi/unlocker.lua
+++ files/luci/model/cbi/unlocker.lua
@@ -3,15 +3,47 @@
 
 -- [REQUIRED STUFF] -----------------------------------------------------------
 require "ubus"
+---checks if a string represents an ip address
+-- @return true or false
+function isIpAddressOrSubnet(ip)
+ if not ip then return false end
+ local a,b,c,d,e=ip:match("^(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)/?(%d?%d?)$")
+ a=tonumber(a)
+ b=tonumber(b)
+ c=tonumber(c)
+ d=tonumber(d)
+ e=tonumber(e)
+ if not a or not b or not c or not d then return false end
+ if a<0 or 255<a then return false end
+ if b<0 or 255<b then return false end
+ if c<0 or 255<c then return false end
+ if d<0 or 255<d then return false end
+ 
+ if e==nil then
+ 	return "ip"
+ elseif e>0 and e<33 then
+ 	return "subnet"
+ else
+ 	return false
+ end
+end
 -------------------------------------------------------------------------------
 
 -- [GLOBAL VARS] --------------------------------------------------------------
 local torrc = "/etc/tor/torrc"
-local torrcSampleConfig = 'User tor\n' ..
+local customIP4 = "/etc/unlocker.ipv4custom"
+local customIP4IPfile = "/etc/unlocker.ipv4ip"
+local customIP4IPheader = "create custom_ip hash:ip family inet maxelem 65536\n"
+local customIP4NETfile = "/etc/unlocker.ipv4net"
+local customIP4NETheader = "create custom_net hash:net family inet maxelem 65536\n"
+local makeTorConfigButtonPressed = false
+local torrcSampleConfig = 'Log notice syslog\n' ..
+						  'DataDirectory /var/lib/tor\n' ..
+						  'User tor\n' ..
 						  'AutomapHostsOnResolve 1\n' ..
 						  'TransPort 0.0.0.0:9040\n' ..
 						  'StrictNodes 1\n' ..
-						  'ExcludeExitNodes {ru},{ua},{kz},{by}\n'
+						  'ExcludeExitNodes {ru},{ua},{kz},{by}'
 
 local fontred = "<font color=\"red\">"
 local fontgreen = "<font color=\"green\">"
@@ -23,6 +55,8 @@ local helpicon = '<span class="cbi-value-helpicon"><img src="/luci-static/reso'.
 				 'urces/cbi/help.gif" alt="help" /></span>'
 local torrcHelp = translate("Exclude your country exit nodes in config. And add TransPort.") ..
 				  brtag .. translate("Example: ") .. bold .. brtag ..
+				  "Log notice syslog" .. brtag ..
+				  "DataDirectory /var/lib/tor" .. brtag ..
 				  "User tor" .. brtag ..
 				  "AutomapHostsOnResolve 1" .. brtag ..
 				  "TransPort 0.0.0.0:9040" .. brtag ..
@@ -77,55 +111,142 @@ enabled = s:taboption("basic",Flag, "enabled", translate("Enable Unlocker"))
 enabled.rmempty = false
 
 iplists = s:taboption("basic",StaticList,"iplists",translate("IPv4 Bypass Lists"),
-					  translate("Multiple selection is allowed"))
+					  translate("Multiple selection is allowed."))
 iplists.widget = "select"
 iplists:depends("enabled","1")
-iplists:value("RKNIPLIST",translate("Roscomnadzor IPv4 (Russia)"))
-iplists:value("GFWIPLIST",translate("Great Firewall IPv4 (China)"))
-iplists:value("CUSTOMIPLIST",translate("Custom IPv4 (TODO)"))
+iplists:value("RKNIPLIST",translate("Roscomnadzor (Russia)"))
+--iplists:value("GFWIPLIST",translate("Great Firewall IPv4 (China)"))
+iplists:value("CUSTOMIPLIST",translate("Custom IPv4"))
+
+--dnlists = s:taboption("basic",StaticList,"dnlists",translate("Domain Bypass Lists"),
+--					  translate("Requires dnsmasq-full! " ..
+--					  			"Multiple selection is allowed."))
+--dnlists.widget = "select"
+--dnlists:depends("enabled","1")
+--dnlists:value("CUSTOMDNLIST",translate("Custom Domain List"))
+
+
 proxy_mode = s:taboption("basic",ListValue, "proxy_mode", translate("Bypass Mode"),
 		translate("TOR is preferred and free but can't deal with UDP traffic"))
 proxy_mode:value("D", translate("Disabled"))
-proxy_mode:value("T", translate("Use TOR Network (TCP Only)"))
-proxy_mode:value("S", translate("Use ShadowSocks (TODO)"))
+proxy_mode:value("T", translate("Use Tor network"))
+--proxy_mode:value("S", translate("Use ShadowSocks (TODO)"))
 -------------------------------------------------------------------------------
 
 -- [ADVANCED LISTS SETTINGS TAB] ----------------------------------------------
 s:tab("listsettings",  translate("Advanced IP-List Settings"))
-rknListStatus = s:taboption("listsettings",DummyValue,"rknListStatus",
-							translate("Roscomnadzor IPv4"))
-rknListStatus.rawhtml = true
-function rknListStatus.cfgvalue(self, section)
-	return bold .. translate("Status: (TODO)") .. endbold
-end
+-- rknListStatus = s:taboption("listsettings",DummyValue,"rknListStatus",
+-- 							translate("Roscomnadzor"))
+-- rknListStatus:depends("iplists","RKNIPLIST")
+-- gfwListStatus:depends("iplists","GFWIPLIST")
+-- rknListStatus.rawhtml = true
+-- function rknListStatus.cfgvalue(self, section)
+-- 	return bold .. translate("Status: (TODO)") .. endbold
+-- end
 
-rknListOptions = s:taboption("listsettings",MultiValue,"rknListOptions"," ",
-				   translate("Currently list is just loaded on boot.") .. brtag ..
-				   translate("Options are just dummy values" ))
+rknListOptions = s:taboption("listsettings",MultiValue,"rknListOptions",
+				   translate("Roscomnadzor (Russia)"))
+--				   translate("Currently list is just loaded on boot."))
+rknListOptions:depends("iplists","RKNIPLIST")
+rknListOptions:depends("iplists","GFWIPLIST")
 rknListOptions:value("RKNLISTCRON",translate("Enable list auto-update"))
 
-gfwListStatus = s:taboption("listsettings",DummyValue,"gfwListStatus",
-							translate("Great Firewall IPv4"))
-gfwListStatus.rawhtml = true
-function gfwListStatus.cfgvalue(self, section)
-	return bold .. translate("Status: (TODO)") .. endbold
-end
+-- gfwListStatus = s:taboption("listsettings",DummyValue,"gfwListStatus",
+-- 							translate("Great Firewall"))
+-- gfwListStatus:depends("iplists","GFWIPLIST")
+-- gfwListStatus:depends("iplists","RKNIPLIST")
+-- gfwListStatus.rawhtml = true
+-- function gfwListStatus.cfgvalue(self, section)
+-- 	return bold .. translate("Status: (TODO)") .. endbold
+-- end
 
-gfwListOptions = s:taboption("listsettings",MultiValue,"gfwListOptions"," ",
-				   translate("Currently list is just loaded on boot.") .. brtag ..
-				   translate("Options are just dummy values"))
-gfwListOptions:value("GFWLISTCRON",translate("Enable list auto-update"))
+-- gfwListOptions = s:taboption("listsettings",MultiValue,"gfwListOptions",
+-- 				   translate("Great Firewall IPv4 (China)"))
+-- gfwListOptions:depends("iplists","RKNIPLIST")
+-- gfwListOptions:depends("iplists","GFWIPLIST")
+-- gfwListOptions:value("GFWLISTCRON",translate("Enable list auto-update"))
 
 customListOptions = s:taboption("listsettings",TextValue, "customListOptions",
-	translate("Custom IPv4"))
+	translate("Custom IPv4"),translate("Example:") .. brtag .. bold ..
+							 "123.123.123.123" .. brtag ..
+							 "143.143.143.143" .. brtag ..
+							 "111.111.111.0/24" .. endbold
+							 )
+customListOptions:depends("iplists","RKNIPLIST")
+customListOptions:depends("iplists","GFWIPLIST")
+customListOptions:depends("iplists","CUSTOMIPLIST")
 customListOptions.rmempty=true
 customListOptions.rows=15
 customListOptions.wrap = "on"
 
+-- function customListOptions.cfgvalue(self, section)
+-- 	return translate("Not ready yet...")
+-- end
+-- function customListOptions.write(self, section, value)
+-- end
+
 function customListOptions.cfgvalue(self, section)
-	return translate("Not ready yet...")
+	if nixio.fs.access(customIP4) then
+		return nixio.fs.readfile(customIP4)
+	else
+		return ""
+	end
 end
 function customListOptions.write(self, section, value)
+	if value == nil or value == '' then 
+		nixio.fs.writefile(customIP4, "")
+		if nixio.fs.access(customIP4NETfile) then
+			nixio.fs.remove(customIP4NETfile)
+		end
+		if nixio.fs.access(customIP4IPfile) then
+			nixio.fs.remove(customIP4IPfile)
+		end
+		if nixio.fs.access(customIP4) then
+			nixio.fs.remove(customIP4)
+		end
+	--elseif nixio.fs.access(customIP4) then
+	else
+		value = value:gsub("\r\n?", "\n")
+		newvalue,customIPValue,customIPNETValue="","",""
+		for line in value:gmatch("[^\r\n]+") do
+			test = isIpAddressOrSubnet(line)
+			if test == "ip" then
+				newvalue = newvalue .. line .. "\n"
+				customIPValue = customIPValue .. "add custom_ip " .. line .. "\n"
+			elseif test == "subnet" then
+				newvalue = newvalue .. line .. "\n"
+				customIPNETValue = customIPNETValue .. "add custom_net " .. line .. "\n"
+			end
+		end
+		value=newvalue
+		if nixio.fs.access(customIP4) then
+			local old_value = nixio.fs.readfile(customIP4)
+		else
+			local old_value = "Not empty"
+		end
+
+		if value ~= old_value then
+			if value ~= "" then
+				nixio.fs.writefile(customIP4, value)
+			elseif nixio.fs.access(customIP4) then
+				nixio.fs.remove(customIP4)
+			end
+			if customIPValue ~= "" then
+				customIPValue = customIP4IPheader .. customIPValue
+				nixio.fs.writefile(customIP4IPfile, customIPValue)
+			elseif nixio.fs.access(customIP4IPfile) then
+				nixio.fs.remove(customIP4IPfile)
+			end			
+			if customIPNETValue ~= "" then
+				customIPNETValue = customIP4NETheader .. customIPNETValue
+				nixio.fs.writefile(customIP4NETfile, customIPNETValue)
+			elseif nixio.fs.access(customIP4NETfile) then
+				nixio.fs.remove(customIP4NETfile)
+			end
+		end
+	-- elseif not nixio.fs.access(customIP4) then
+	-- 	nixio.fs.writefile(customIP4, value)
+	end
 end
 
 -------------------------------------------------------------------------------
@@ -180,16 +301,18 @@ if torBinary ~= "" then
 			luci.http.redirect(luci.dispatcher.build_url("admin", "services", "unlocker"))
 		end
 	end
-	torrcButtonConfig = s:taboption("torConfig",Button,translate("Make me Tor Config")," ",
-									translate("Creates a simple Tor Config for unlocker"))
-	torrcButtonConfig:depends("proxy_mode","T")
-	torrcButtonConfig.inputtitle=translate("Make me Tor Config")
-	torrcButtonConfig.inputstyle="apply"
-	function torrcButtonConfig.write() 
-		luci.sys.exec("echo -e \"" .. torrcSampleConfig .. "\" > " .. torrc)
-		luci.sys.exec("sleep 1")
-		luci.http.redirect(luci.dispatcher.build_url("admin", "services", "unlocker"))
-	end
+	if nixio.fs.access(torrc) then
+		torrcButtonConfig = s:taboption("torConfig",Button,translate("Make me Tor config")," ",
+										translate("Creates a simple Tor config for unlocker"))
+		torrcButtonConfig:depends("proxy_mode","T")
+		torrcButtonConfig.inputtitle=translate("Make me Tor config")
+		torrcButtonConfig.inputstyle="apply"
+		function torrcButtonConfig.write()
+			makeTorConfigButtonPressed = true
+			nixio.fs.writefile(torrc, torrcSampleConfig)
+			luci.http.redirect(luci.dispatcher.build_url("admin", "services", "unlocker"))
+		end
+	end	
 else
 	torrcButtonInstall = s:taboption("torConfig",Button,"Install Tor",
 						" ","Please press ONLY ONCE and wait (approx 20 sec)")
@@ -224,7 +347,7 @@ if nixio.fs.access(torrc) then
 		elseif nixio.fs.access(torrc) then
 			value = value:gsub("\r\n?", "\n")
 			local old_value = nixio.fs.readfile(torrc)
-			if value ~= old_value then
+			if value ~= old_value and not makeTorConfigButtonPressed then
 				nixio.fs.writefile(torrc, value)
 			end
 		end
