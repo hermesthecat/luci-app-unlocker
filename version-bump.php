a89165a8b317eb841d4f3e85ba022382596799d3
Alex D <79151006869@live.ru>
2018-10-20 23:44:57 +0300

    version bump tweak

--- version-bump.php
+++ version-bump.php
@@ -7,7 +7,7 @@ $file=file_get_contents(__DIR__.DIRECTORY_SEPARATOR."Makefile");
 preg_match('/PKG_VERSION:=(.*)/', $file,$version_number);
 $version_number=$version_number[1];
 //echo "$version_number\n";
-$file=preg_replace('/PKG_VERSION:=(.*)/', "PKG_VERSION:=".($version_number+0.1), $file);
+$file=preg_replace('/PKG_VERSION:=(.*)/', "PKG_VERSION:=".($version_number+0.01), $file);
 preg_match('/PKG_RELEASE:=(.*)/', $file,$pkg_number);
 $pkg_number=$pkg_number[1];
 //echo "$pkg_number\n";
